854e6f05492c8bd65b228e61fc3fcec877aa6258
David Rios <david.rios@upessencia.com.br>
2017-06-29 16:10:09 -0300

    added custom bash prompt

---
+++ bash/prompt.sh
@@ -0,0 +1,14 @@
+# A two-line colored Bash prompt (PS1) with Git branch and a line decoration
+# Original version by Michal Kottman, 2012
+ 
+RESET="\[\033[0m\]"
+RED="\[\033[0;31m\]"
+GREEN="\[\033[01;32m\]"
+BLUE="\[\033[01;34m\]"
+YELLOW="\[\033[0;33m\]"
+ 
+PS_INFO="$GREEN\u@\h$RESET:$BLUE\w"
+PS_GIT='$(__git_ps1 " '${YELLOW}'[%s]")'
+PS_TIME="\[\033[\$((COLUMNS-10))G\] $RED[\t]"
+
+export PS1="${PS_INFO}${PS_GIT} ${PS_TIME}\n${RESET}\$ "
