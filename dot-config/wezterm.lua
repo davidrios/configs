7a353d879fb61f5496855db53a28a86bd6688d14
David Rios <david.rios.gomes@gmail.com>
2022-12-12 01:59:12 -0300

    Update

--- dot-config/wezterm.lua
+++ dot-config/wezterm.lua
@@ -2,23 +2,27 @@ local wezterm = require "wezterm";
 
 local font = wezterm.font("DejaVu Sans Mono");
 local default_prog = nil
-local launch_menu = nil
-local window_decorations = "RESIZE"
+local launch_menu = {}
+local window_decorations = nil
+local canonicalize_pasted_newlines = nil
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
   font = wezterm.font("Consolas");
+  window_decorations = "RESIZE"
+  canonicalize_pasted_newlines = "None"
 
-  launch_menu = {}
   default_prog = {"wsl.exe"}
 
   table.insert(launch_menu, {
     label = "CMD",
     args = {"cmd.exe"},
+    domain = {DomainName="local"},
   })
 
   table.insert(launch_menu, {
     label = "PowerShell",
     args = {"powershell.exe"},
+    domain = {DomainName="local"},
   })
 
   -- Find installed visual studio version(s) and add their compilation
@@ -42,6 +46,7 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
       table.insert(launch_menu, {
         label = "x64 Native Tools VS " .. year,
         args = args,
+        domain = {DomainName="local"},
       })
     end
   end
@@ -50,6 +55,7 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
     table.insert(launch_menu, {
       label = "Git Bash",
       args = {"C:/Program Files/Git/usr/bin/bash.exe", "-i", "-l"},
+      domain = {DomainName="local"},
       set_environment_variables = {
       },
     })
@@ -80,7 +86,8 @@ local colors = {
   -- The default text color
   foreground = "#dedede",
   -- The default background color
-  background = "#0C0C0C",
+  background = "#0c0c0c",
+  -- window_padding = "red",
 
   ansi = {
     "#0C0C0C",
@@ -133,9 +140,18 @@ local colors = {
   -- }
 }
 
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local title = wezterm.truncate_right(tab.active_pane.title, max_width)
+  return {
+    {Text=string.format("%s: %s", tab.tab_index + 1, title)},
+  }
+end)
+
 keys = {
   {key="t", mods="CTRL|ALT", action="ShowLauncher"},
-  {key="w", mods="CTRL|ALT", action=wezterm.action{CloseCurrentTab={confirm=false}}}
+  {key="w", mods="CTRL|ALT", action=wezterm.action{CloseCurrentTab={confirm=false}}},
+  {key="h", mods="CTRL|ALT", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+  {key="v", mods="CTRL|ALT", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
 }
 for i=0,9 do
   table.insert(keys, {key=i < 9 and tostring(i + 1) or "0", mods="ALT", action=wezterm.action{ActivateTab=i}})
@@ -145,6 +161,7 @@ return {
   font = font,
   font_size = 11,
   enable_scroll_bar = true,
+  -- scroll_bar_mode = 'AllPanes',
   scrollback_lines = 20000,
   colors = colors,
   audible_bell = "Disabled",
@@ -155,8 +172,31 @@ return {
   launch_menu = launch_menu,
   default_prog = default_prog,
   keys = keys,
+  clean_exit_codes = {130, 255},
   window_decorations = window_decorations,
-  initial_cols = 154,
-  initial_rows = 44,
-  enable_wayland = true,
+  canonicalize_pasted_newlines = canonicalize_pasted_newlines,
+  -- window_background_opacity = 0.8,
+  -- mouse_bindings = {
+  --   {
+  --     mods="NONE",
+  --     event={Down={streak=1, button="Right"}},
+  --     action=require"wezterm".action{ExtendSelectionToMouseCursor="Cell"}
+  --   },
+  --   {
+  --     mods="NONE",
+  --     event={Down={streak=2, button="Right"}},
+  --     action=require"wezterm".action{ExtendSelectionToMouseCursor="Word"}
+  --   },
+  -- },
+  -- initial_cols = 140,
+  -- initial_rows = 30,
+  -- pane_focus_follows_mouse = true,
+  -- use_fancy_tab_bar = false,
+  -- tab_bar_at_bottom = true,
+  window_padding = {
+    -- left = "0px",
+    -- right = "0px",
+    -- top = "0px",
+    -- bottom = "0px",
+  }
 }
